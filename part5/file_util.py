06c3d58a7d413eb38345e9eb37e917b65a1de9f0
Tomer Cnaan <cnaant73@gmail.com>
2018-05-27 16:37:01 +0300

    Create file_util.py

---
+++ part5/file_util.py
@@ -0,0 +1,8 @@
+import os
+
+def file_generator(root, ext=""):
+    for x in os.listdir(root):
+        if os.path.isfile(root + "\\" + x) and x.endswith(ext):
+            yield root + "\\" + x
+
+
